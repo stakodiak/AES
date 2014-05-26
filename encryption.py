0ad83c73df02cdf0db190b1e102973a159c6accd
alex <astac1@umbc.edu>
2014-05-26 17:34:55 -0400

    Utilities for encrypting and decrypting text.

--- encryption.py
+++ encryption.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 # coding=utf-8
 import getpass
+import sys
 from Crypto.Cipher import AES
 
     
@@ -9,12 +10,32 @@ def pad_string(text, block_size=32, padding=' '):
     padded = text + (block_size - len(text) % block_size) * padding
     return padded
 
+def encrypt(cipher, data):
+    """Encrypts `data` with `cipher`."""
+    padded = pad_string(data)
+    encrypted = cipher.encrypted(padded)
+    return encrypted
+
+def decrypt(cipher, encrypted_data):
+    """Decrypt AES-encrypted data with `cipher`."""
+    decrypted = cipher.decrypt(encrypted_data)
+    return decrypted
+
 def main():
     # Create cipher from padded password.
     pwd = getpass.getpass()
     key = pad_string(pwd)
     cipher = AES.new(key)
 
+    # Encrypt given text.
+    data = sys.stdin.read()
+    padded_data = pad_string(data)
+    encrypted = cipher.encrypt(padded_data)
+
+    print encrypted
+
+    decrypted = decrypt(cipher, encrypted)
+    print decrypted
     print "'{}'".format(key), len(key)
 
 if __name__ == '__main__':
